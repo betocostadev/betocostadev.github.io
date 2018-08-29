ed9cfb802ad9958cf572cf625af710efbd8fba53
Beto Costa <robertomarx@gmail.com>
2018-08-29 17:48:46 -0300

    more to the structure

--- js/landing_script.js
+++ js/landing_script.js
@@ -1,28 +1,17 @@
 /* eslint-disable */
 (function () {
 
-  // let myButton = document.getElementById('myButton');
-  //   myButton.addEventListener('click', function () {
-  //       incrementCounter();
-  //       updateUI();
-  //   });
-
   const knowMoreBtn = document.getElementById('down-btn');
 
   knowMoreBtn.addEventListener('click', function () {
     const aboutSect = document.getElementById('about');
     aboutSect.scrollIntoView({ behavior: 'smooth' });
-    // this.classList.add('croxura');
-    // alert('Hi!')
+
   })
 
-  // function scrollToAbout() {
-  //   const aboutSect = document.getElementById('about');
-  //   // Scroll to a certain element
-  //   aboutSect.classList.add('croxura');
-  //   // aboutSect.scrollIntoView({
-  //   //   behavior: 'smooth',
-  //   // });
-  // };
+  const hello = document.getElementById('helloDiv');
+  hello.addEventListener("animationend", function() {
+    hello.classList.add('disabled');
+  }, 3000);
 
 }());
