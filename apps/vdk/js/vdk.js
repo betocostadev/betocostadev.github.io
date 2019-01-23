08a7c795e0acc79bf8cb45da3772c110d2faff8c
Beto Costa <robertomarx@gmail.com>
2019-01-23 19:46:43 -0200

    added toggle social menu

--- apps/vdk/js/vdk.js
+++ apps/vdk/js/vdk.js
@@ -1,1 +1,15 @@
-// Not started
+// Virtual Drum Kit
+const shareBtn = document.getElementById('shareBtn');
+const social = document.getElementsByClassName('social')[0];
+const shareBack = document.getElementById('shareBack');
+
+shareBtn.addEventListener('click', () => {
+  if (social.classList.contains('open')) {
+    social.classList.remove('open');
+    shareBack.style.fill = '#fff';
+  } else {
+    social.classList.add('open');
+    // shareBtn.setAttribute('style', 'path', 'fill:red');
+    shareBack.style.fill = 'yellow';
+  }
+});
