f50b08b7c7c0cb2082708ff9d1acdace43b6dc76
Beto Costa <robertomarx@gmail.com>
2019-02-19 20:15:15 -0300

    beta done

--- apps/memgame/js/app.js
+++ apps/memgame/js/app.js
@@ -1,5 +1,5 @@
-/* global document: true window: true */
-// Buttons
+/* global document: true */
+
 const startBtn = document.getElementById('startGame');
 const aboutBtn = document.getElementById('about');
 const gameSection = document.getElementById('game');
@@ -8,15 +8,28 @@ const body = document.getElementsByTagName('body');
 const startCounter = document.getElementById('startCounter');
 const status = document.getElementById('status');
 const movesDisplay = document.getElementById('movesDisplay');
+const endMovesDisplay = document.getElementById('totalMoves');
+const endMovesLoser = document.getElementById('totalMovesLoser');
+const doomContainer = document.getElementById('doom');
+const doomDisplay = document.getElementById('doomDisplay');
 
 const landing = document.getElementById('landing');
 const closeAbout = document.getElementById('closeAboutModal');
 const modalAbout = document.getElementById('modal-about');
+const modalWinner = document.getElementById('modal-winner');
+const modalLoser = document.getElementById('modal-loser');
 const memHeading = document.getElementById('memHeading');
 
+const flipCardSound = document.getElementById('flipCardSnd');
+const flipBackSound = document.getElementById('flipBackSnd');
+const successSound = document.getElementById('sucessSnd');
+
 let moves = 0;
+let correct = 0;
 let cards = [];
 let cardsrc = [];
+let imgsrcCheck = [];
+let win = false;
 
 aboutBtn.addEventListener('click', () => {
   modalAbout.classList.toggle('modal-display');
@@ -32,48 +45,50 @@ closeAbout.addEventListener('click', () => {
   }, 900);
 });
 
-function startGame() {
-  memHeading.classList.toggle('hide');
-  status.classList.remove('hide');
-  status.classList.add('status');
-  const startcards = Array.from(document.getElementsByClassName('card'));
-  startcards.forEach((card) => {
-    card.classList.add('card-hide');
-    card.firstChild.classList.add('img-hide');
-  });
-  setTimeout(() => {
-    startcards.forEach((card) => {
-      card.classList.remove('correct');
-      card.classList.add('hidden-card');
-    });
-  }, 450);
+function winner() {
+  correct += 1;
+  if (correct === 16) {
+    setTimeout(() => {
+      modalWinner.classList.toggle('modal-display');
+      modalWinner.classList.add('displayAnim');
+      endMovesDisplay.textContent = moves;
+      doomContainer.classList.add('hide');
+      win = true;
+      if (moves < 25) {
+        endMovesDisplay.classList.add('good');
+      }
+      // console.log('nothing');
+    }, 400);
+  }
 }
 
 function compareCards() {
-  console.log(cards);
-  console.log(cardsrc);
   cards.sort();
-  if (cards[0] === cards[1]) {
-    console.log('Certo, yes!');
+  if (cards[0] === cards[1] && (imgsrcCheck[0] !== imgsrcCheck[1])) {
+    // console.log('Certo, yes!');
     setTimeout(() => {
       cardsrc.forEach((card) => {
         card.classList.remove('card-show');
         card.classList.add('card-correct');
         card.classList.add('correct');
+        card.firstChild.removeEventListener('click', checker);
+        successSound.play();
+        winner();
+        imgsrcCheck = [];
       });
       cardsrc = [];
-    }, 800);
-    console.log(cardsrc[0]);
-    console.log(cardsrc[1]);
+    }, 500);
+    // console.log(cardsrc[0]);
+    // console.log(cardsrc[1]);
   } else {
-    console.log('errado!');
+    // console.log('errado!');
     setTimeout(() => {
       cardsrc.forEach((card) => {
         card.classList.remove('card-show');
         card.classList.add('card-incorrect');
-        // card.classList.add('correct');
+        flipBackSound.play();
       });
-    }, 800);
+    }, 500);
     setTimeout(() => {
       cardsrc.forEach((card) => {
         card.classList.add('card-hide', 'hidden-card');
@@ -81,36 +96,61 @@ function compareCards() {
         card.firstChild.classList.add('img-hide');
       });
       cardsrc = [];
-    }, 1200);
+    }, 900);
   }
   cards = [];
-  // cardsrc = [];
+  imgsrcCheck = [];
 }
 
 function checker(e) {
   const card = e.target.parentNode;
   const firstCard = e.target.attributes.name.nodeValue;
+  const imgCheck = e.target.attributes.src.nodeValue;
   e.target.classList.remove('img-hide');
   e.target.classList.add('img-show');
   card.classList.remove('card-hide', 'hidden-card');
   card.classList.add('card-show');
+  imgsrcCheck.push(imgCheck);
   cards.push(firstCard);
   cardsrc.push(card);
   moves += 1;
   movesDisplay.textContent = moves;
-  console.log(moves);
+  // console.log(moves);
+  flipCardSound.play();
   if (cards.length === 2) {
     compareCards();
   }
-  console.log(cards);
+}
+
+function endGame() {
+  modalLoser.classList.remove('modal-display');
+  modalLoser.classList.add('displayAnim');
+  endMovesLoser.textContent = moves;
+}
+
+function loser() {
+  setInterval(() => {
+    const timeIsUp = doomDisplay.textContent;
+    doomDisplay.classList.add('good');
+    if (timeIsUp === '00:15') {
+      doomDisplay.classList.remove('good');
+      doomDisplay.classList.add('danger');
+    } else if ((timeIsUp === '00:00') && (!win)) {
+      endGame();
+    }
+  }, 1000);
 }
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
   const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-  console.log(display);
+  // console.log(display);
   startCounter.textContent = display;
+  doomDisplay.textContent = display;
+  if (display === '01:30') {
+    loser(display);
+  }
 }
 
 let countdown;
@@ -129,10 +169,38 @@ function timer(seconds) {
   }, 1000);
 }
 
-function startTimer() {
-  const seconds = parseInt(6, 10);
+function doomTime(time) {
+  const seconds = parseInt(time, 10);
   timer(seconds);
-  setTimeout(() => { startGame(); }, 6000);
+}
+
+function startGame() {
+  doomTime(80);
+  doomContainer.classList.remove('hide');
+  const cardImages = Array.from(gameGrid.querySelectorAll('div.card > img'));
+  cardImages.forEach((cardImg) => {
+    cardImg.addEventListener('click', checker);
+  });
+  memHeading.classList.toggle('hide');
+  status.classList.remove('hide');
+  status.classList.add('status');
+  const startcards = Array.from(document.getElementsByClassName('card'));
+  startcards.forEach((card) => {
+    card.classList.add('card-hide');
+    card.firstChild.classList.add('img-hide');
+  });
+  setTimeout(() => {
+    startcards.forEach((card) => {
+      card.classList.remove('correct');
+      card.classList.add('hidden-card');
+    });
+  }, 450);
+}
+
+function startTimer() {
+  const seconds = parseInt(5, 10);
+  timer(seconds);
+  setTimeout(() => { startGame(); }, 5000);
 }
 
 function renderGame() {
@@ -198,7 +266,6 @@ function renderGame() {
         break;
       default:
     }
-    cardImg.addEventListener('click', checker);
     gameGrid.appendChild(card);
     card.appendChild(cardImg);
   }
