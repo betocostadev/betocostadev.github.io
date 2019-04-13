427cff981f4325906e4e53af329184f71f162cd3
Beto Costa <robertomarx@gmail.com>
2019-04-13 15:03:02 -0300

    ui mostly done

--- apps/liveuni-fend-test2/public/scripts/main.js
+++ apps/liveuni-fend-test2/public/scripts/main.js
@@ -1,7 +1,10 @@
 /* global document: true */
+// TODO: Create items based on Items
+
 const option = document.getElementById('option');
 const subMenu = document.getElementById('submenu');
 const slideWidth = subMenu.offsetWidth;
+const qtd = document.getElementsByTagName('input')[0];
 // $0.textContent.toUpperCase()
 
 function showOptions() {
@@ -15,4 +18,17 @@ function showOptions() {
 //   subMenu.classList.toggle('slide');
 }
 
+function addQtd(value) {
+  console.log(`O valor é ${value}`);
+}
+
+function addItems() {
+  if (qtd.value === '' || isNaN(qtd.value)) {
+    console.log('Sem valor, ou não é um número!');
+  } else {
+    addQtd(qtd.value);
+  }
+}
+
 option.addEventListener('click', showOptions);
+window.addEventListener('click', addItems);
